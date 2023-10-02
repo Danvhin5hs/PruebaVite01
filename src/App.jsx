5849ac29d5254dfb62ff7b2588e445d5cf463e69
Danvhin5hs <Vramgallerdi1986puterxd@gmail.com>
2023-10-02 00:46:00 -0500

    Cambios en el server git

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export function App(){
+    return( 
+    
+     <article /* Forma css directa  style={{display:"flex", alignItems:"center", color: "#fff"}} */ >
+        <header>
+            <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" alt="" style={{width:"100px"}}/>
+            <div>
+                <strong>
+                    Miguel Angel Duran
+                </strong>
+                <span> @Miduran </span>
+            </div>
+        </header>
+        <aside>
+            <button>
+                Seguir
+            </button>
+        </aside>
+     </article>
+    )
+}
